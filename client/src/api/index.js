99e80d0954fa4311c685cd4a8ac4d873b9d87e5e
NeelamKishor <121456070+NeelamKishor@users.noreply.github.com>
2024-10-17 18:15:29 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = process.env.REACT_APP_API_URL || 'http://localhost:5000/posts';
+const url = 'https://social-media-web-backend.onrender.com';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
