7da93c286751f8e7a3edf06bc6eadef570460af3
NeelamKishor <121456070+NeelamKishor@users.noreply.github.com>
2024-10-17 15:09:32 +0530

    First commit Social Media Web

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+
+dotenv.config(); // Load environment variables from .env file
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// Use environment variables from the .env file
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
+
+//$env:NODE_OPTIONS = "--openssl-legacy-provider"
