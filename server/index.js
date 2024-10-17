b0d1927cda89de676a372c8d6a3f675504a31913
NeelamKishor <121456070+NeelamKishor@users.noreply.github.com>
2024-10-17 15:49:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use('/posts', postRoutes);
 
 // Use environment variables from the .env file
 const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
